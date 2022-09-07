27318ae9b89dcc0b6ad75388e0df36b2ba024040
tannguyen <110894101+buvghhj120902@users.noreply.github.com>
2022-09-07 20:40:13 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+
+
+
+
+const navOpenBtn =  document.querySelector('[data-menu-open-btn]');
+const navCloseBtn = document.querySelector('[data-menu-close-btn]');
+const navbar = document.querySelector('[data-navbar]');
+const overlay = document.querySelector('[data-overlay]');
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+for(let i = 0; i < navElemArr.length; i++){
+    navElemArr[i].addEventListener("click", function(){
+        navbar.classList.toggle("active");
+         overlay.classList.toggle("active");
+        document.body.classList.toggle("active");
+    });
+}
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function(){
+
+       window.scrollY >= 10 ? header.classList.add("active") :header.classList.remove("active");
+
+});
+
+
+
+
+
+
+
+
+
+
+const goToBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function(){
+    
+    this.window.scrollY >= 500 ? goToBtn.classList.add("active") : goToBtn.classList.remove("active");
+
+});
